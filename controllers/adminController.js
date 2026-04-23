783513dcf77498ae197d744885ecdce305784407
Kang MarguZ <kangmarguz@gmail.com>
2026-04-23 13:21:04 +0700

    update to api reserving history for user.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -23,3 +23,72 @@ export const getListStats = async (req, res, next) => {
         next(error);
     }
 };
+
+export const getListReservations = async (req, res, next) => {
+    try {
+        const { id } = req.user;
+        const campings = await prisma.landmark.count({
+            where: {
+                profileId: id,
+            },
+        });
+
+        const total = await prisma.booking.aggregate({
+            where: {
+                profileId: id,
+            },
+            _sum: {
+                totalNights: true,
+                totalPrice: true,
+            },
+        });
+
+        res.json({
+            status: {
+                code: '200',
+                message: 'Get List Stats Success',
+            },
+            result: {
+                count: campings,
+                totalNights: total._sum.totalNights,
+                totalPrice: total._sum.totalPrice,
+            },
+        });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
+
+export const getAllListReservations = async (req, res, next) => {
+    console.log("get own list reservation... !");
+    
+    try {
+        const { id } = req.user;
+        const reservation = await prisma.booking.findMany({
+            where: {
+                paymentStatus: true,
+                landmark: {
+                    profileId: id,
+                }
+            },
+            include: {
+                landmark: {
+                    select: {
+                        id:true,
+                        title: true,
+                        price: true,
+                    }
+                }
+            },
+            orderBy: {
+                createdAt: 'desc',
+            }
+        });
+
+        res.json({ result: reservation });
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
 import { authCheck } from '../middlewares/auth.js';
-import { getListStats } from '../controllers/adminController.js';
+import { getListReservations, getListStats, getAllListReservations } from '../controllers/adminController.js';
 
 router.get('/listStats', authCheck, getListStats);
+router.get('/listReservations', authCheck, getListReservations);
+router.get('/get-all-reservations', authCheck, getAllListReservations);
 
 export default router;
